5f3a50f71315f10542d7b61e47463a5e0240dffe
jaejinkim121 <gisgen9@snu.ac.kr>
2022-07-15 21:18:54 +0900

    Add loading IMU data from pre-processed data

--- src/clinical_trial_analysis/analysis.py
+++ src/clinical_trial_analysis/analysis.py
@@ -1,7 +1,7 @@
-from common.load_xls import load_xls
-from include.sole_header import folder_path_name, force_sensor_sync
-from include.sole_header import load_GRF, load_SENSOR_vol
-
+from include.load_imu_data import load_xls, load_imu
+from include.sole_sensor_preprocessing import folder_path_name, force_sensor_sync
+from include.sole_sensor_preprocessing import load_GRF, load_SENSOR_vol
+from include.config import PlotFlag
 
 MAX_TRIAL = 100
 
@@ -20,12 +20,7 @@ def get_full_file_path(prefix, suffix, index):
     return path
 
 
-"""
-이민희 잘해라
-"""
-
-
-def lmh(trial_num, walk_num):
+def get_dataframe_sole_sensor(trial_num, walk_num):
 
     path = '../../data/RH-%s/' % (str(trial_num))
     force_sync_path = '../../data/analyzed/sole/df_sync_force.csv'
@@ -34,7 +29,7 @@ def lmh(trial_num, walk_num):
     # load sync time
     (force_start_time, L_sensor_start_time,
      R_sensor_start_time) = force_sensor_sync(
-         force_sync_path, sensor_sync_path, trial_num, walk_num)
+        force_sync_path, sensor_sync_path, trial_num, walk_num)
 
     # modify walk num (string)
     if (len(str(walk_num)) == 1):
@@ -43,8 +38,10 @@ def lmh(trial_num, walk_num):
         walk_num = str(walk_num)
 
     # GRF dataframe, end time
-    (GRF_file, GRF_name) = folder_path_name(path, "end", "WALK%s.XLS" % (
-        walk_num), 1)
+    (GRF_file, GRF_name) = folder_path_name(
+        path, "end",
+        "WALK%s.XLS" % walk_num,
+        T_F=1)
     (df_didim_GRF, GRF_end_time) = load_GRF(GRF_file[0])
 
     # sensor path, end time
@@ -55,76 +52,58 @@ def lmh(trial_num, walk_num):
     vol = ['time', 'v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7']
 
     # sensor L, R path
-    (L_walk_data_list,
-     L_walk_name_list) = folder_path_name(sensor_path, "start", "L", 1)
-    (R_walk_data_list,
-     R_walk_name_list) = folder_path_name(sensor_path, "start", "R", 1)
+    (L_walk_data_list, L_walk_name_list) = \
+        folder_path_name(sensor_path, "start", "L", 1)
+    (R_walk_data_list, R_walk_name_list) = \
+        folder_path_name(sensor_path, "start", "R", 1)
 
     # L dataframe
     df_vol_L = load_SENSOR_vol(L_walk_data_list[0], trial_num)
-    df_vol_L = df_vol_L.loc[(df_vol_L["time"] >= L_sensor_start_time) & (
-        df_vol_L["time"] <= L_sensor_end_time)][vol]
+    df_vol_L = df_vol_L.loc[
+        (df_vol_L["time"] >= L_sensor_start_time) &
+        (df_vol_L["time"] <= L_sensor_end_time)
+        ][vol]
     # initialize L time
     df_vol_L["time"] = df_vol_L["time"] - L_sensor_start_time
 
     # R dataframe
     df_vol_R = load_SENSOR_vol(R_walk_data_list[0], trial_num)
     df_vol_R = df_vol_R.loc[(df_vol_R["time"] >= R_sensor_start_time) & (
-        df_vol_R["time"] <= R_sensor_end_time)][vol]
+            df_vol_R["time"] <= R_sensor_end_time)][vol]
     # initialize R time
     df_vol_R["time"] = df_vol_R["time"] - R_sensor_start_time
 
     return df_didim_GRF, df_vol_L, df_vol_R
 
 
-"""
-여기까지
-"""
+def get_dataframe_imu(trial_num, walk_num):
+    # Define File Paths
+    # Fill zero to make directory name a form of RH-##
+    path_trial = '../../data/RH-{}/'.format(str(trial_num).zfill(2))
+    path_trimmed_imu = path_trial + "imu/"
 
-# def kjj(trial_num, walk_num):
-#     # Define File Paths
-#     path_test = '../../data/RH-09/'
-#     index_list = []
+    df_didim_kinematics = load_xls(
+        path_trial + "TRIMMED_WALK{}.xls".format(str(walk_num).zfill(2))
+    )
 
-#     # Use key value for purposes.
-#     # 'device' for our own device data
-#     # 'motion_capture' for didim data
-#     data_type_prefix = {'device': 'data/', 'motion_capture': ''}
-#     file_name_prefix = {'device': 'temp', 'motion_capture': 'TRIMMED_WALK'}
-#     file_name_suffix = {'device': 'temp', 'motion_capture': '.XLS'}
+    df_imu = load_imu(path_trimmed_imu + "trimmed_walk{}_with_imu.xlsx".format(str(walk_num).zfill(2)))
 
-#     # Load data and make dataframe
-#     df_didim = []
-#     key = 'motion_capture'
-
-#     for i in range(1, MAX_TRIAL):
-#         full_path_didim = get_full_file_path(
-#             prefix=[path_test, data_type_prefix[key], file_name_prefix[key]],
-#             suffix=[file_name_suffix[key]],
-#             index=i
-#         )
-#         #
-#         try:
-#             df_didim.append(load_xls(full_path_didim))
-#         except FileNotFoundError:
-#             print("END of reading files")
-#             break
-#     print(len(df_didim))
-#     """
-#     Kinematic Data Plot
-
-#       1. Roll, Pitch, Yaw Angle of 7 limbs.
-
-#     """
-#     #
-
-#     return df_didim_kinematics
+    return df_didim_kinematics, df_imu
 
 
 def main():
+    trial_num, walk_num = 8, 15
 
-    (df_didim_GRF, df_vol_L, df_vol_R) = lmh("07", 20)
+    # ----------------- DATA LOADING ------------------- #
+
+    df_didim_kinematics, df_imu = \
+        get_dataframe_imu(trial_num, walk_num)
+    df_didim_GRF, df_vol_L, df_vol_R = \
+        get_dataframe_sole_sensor(trial_num, walk_num)
+
+    # -------------------  PLOT  ----------------------- #
     
+
     return 0
 
 
