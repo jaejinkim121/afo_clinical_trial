27796ecf61b578f4675480ffa7e072156d581924
jaejinkim121 <gisgen9@snu.ac.kr>
2023-08-14 17:11:22 +0900

    Add individual file skeleton

---
+++ src/trial_report/jj.py
@@ -0,0 +1,28 @@
+from bagpy import bagreader
+import pandas as pd
+
+
+class ClinicalIndexJJ:
+    @staticmethod
+    def get_clinical_index_max_toe_clearance(file_path):
+        clinical_index = 0.0
+
+        ...
+
+        return clinical_index
+
+    @staticmethod
+    def get_clinical_index_gait_speed_imu(file_path):
+        clinical_index = 0.0
+
+        ...
+
+        return clinical_index
+
+    @staticmethod
+    def get_clinical_index_gait_speed_distance(file_path):
+        clinical_index = 0.0
+
+        ...
+
+        return clinical_index
---
+++ src/trial_report/mh.py
@@ -0,0 +1,26 @@
+from bagpy import bagreader
+import pandas as pd
+
+
+class ClinicalIndexMH:
+    @staticmethod
+    def get_symmetry_index_sth(filePath):
+        # Temporary variable for template
+        paretic = 0
+        non_paretic = 0
+        symmetry = 0.55
+
+        ...
+
+        return paretic, non_paretic, symmetry
+
+    @staticmethod
+    def get_symmetry_index_sth(filePath):
+        # Temporary variable for template
+        paretic = 0
+        non_paretic = 0
+        symmetry = 0.55
+
+        ...
+
+        return paretic, non_paretic, symmetry
