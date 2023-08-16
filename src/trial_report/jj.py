667c69b4af9a70ff4ae3b86fe617cdf9bc552067
jaejinkim121 <gisgen9@snu.ac.kr>
2023-08-16 09:38:38 +0900

    Add Kinematics report

--- src/trial_report/jj.py
+++ src/trial_report/jj.py
@@ -1,23 +1,49 @@
 from bagpy import bagreader
 import pandas as pd
+import numpy as np
+import csv
 
 
 class ClinicalIndexJJ:
     @staticmethod
-    def get_clinical_index_max_toe_clearance(file_path):
-        clinical_index = 0.0
+    def get_clinical_index_max_toe_clearance(paretic_path, non_paretic_path):
+        paretic_toe_clearance = []
+        non_paretic_toe_clearance = []
+        # paretic
+        with open(paretic_path) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for csv_row in csv_reader:
+                paretic_toe_clearance.append(float(csv_row[1]))
 
-        ...
+        with open(non_paretic_path) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for csv_row in csv_reader:
+                non_paretic_toe_clearance.append(float(csv_row[1]))
 
-        return clinical_index
+        np_ptc = np.array(paretic_toe_clearance)
+        np_nptc = np.array(non_paretic_toe_clearance)
+
+        paretic_mean = np.mean(np_ptc)
+        paretic_stdev = np.std(np_ptc)
+        non_paretic_mean = np.mean(np_nptc)
+        non_paretic_stdev = np.std(np_nptc)
+        symmetry = paretic_mean / (paretic_mean + non_paretic_mean)
+
+        return (paretic_mean, paretic_stdev,
+                non_paretic_mean, non_paretic_stdev,
+                symmetry
+                )
 
     @staticmethod
-    def get_clinical_index_gait_speed_imu(file_path):
-        clinical_index = 0.0
+    def get_clinical_index_gait_speed_imu(stride_path):
+        stride = []
+        with open(stride_path) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for csv_row in csv_reader:
+                stride.append(float(csv_row[1]))
+        stride = np.array(stride)
 
-        ...
-
-        return clinical_index
+        return np.sum(stride) / 120.0
 
     @staticmethod
     def get_clinical_index_gait_speed_distance(file_path):
